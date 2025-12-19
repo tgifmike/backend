658deb71782f81a8145cce6615aed8eed1463a1c
Mike <mr.mikesorrentino@gmail.com>
2025-12-19 14:38:30 -0500

    Update AccountEntity.java

--- src/main/java/com/backend/backend/entity/AccountEntity.java
+++ src/main/java/com/backend/backend/entity/AccountEntity.java
@@ -14,6 +14,7 @@ import org.springframework.data.annotation.LastModifiedBy;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
+
 import java.time.Instant;
 import java.util.UUID;
 
