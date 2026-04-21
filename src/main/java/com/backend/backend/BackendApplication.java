7f311b8b1377c4831ed6bbe0eeee48ec4bb70f0f
Mike <mr.mikesorrentino@gmail.com>
2026-04-21 13:21:14 -0400

    fix seeing deleted

--- src/main/java/com/backend/backend/BackendApplication.java
+++ src/main/java/com/backend/backend/BackendApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
-@EnableJpaAuditing(auditorAwareRef = "auditorProvider")
+@EnableJpaAuditing(auditorAwareRef = "auditorAware")
 @SpringBootApplication
 @EnableCaching
 public class BackendApplication {
